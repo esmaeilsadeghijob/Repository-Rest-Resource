2a4007344b434e3c164df3d314b872207a8c9f8f
esmaeilsadeghijob <esmaeilsadeghijob@gmail.com>
2023-12-25 12:14:36 +0330

    refactoring

--- src/main/java/com/cando/uni/repository/TeacherRepository.java
+++ src/main/java/com/cando/uni/repository/TeacherRepository.java
@@ -4,7 +4,10 @@ import com.cando.uni.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
 
+@Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+
+    Optional<Teacher> findByTechNO(Long techNO);
 }
